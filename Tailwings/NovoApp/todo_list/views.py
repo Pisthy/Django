8c6998e3dba8f1a867eb0757bd6f298fe90825e1
MARCUS VINICIUS COSTA DA SILVA <marcus.vsilva@a.ucb.br>
2025-06-04 10:54:59 -0300

    Aula

--- Tailwings/NovoApp/todo_list/views.py
+++ Tailwings/NovoApp/todo_list/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import todo
+from .forms import TodoForm
+
 # Create your views here.
 def home(request):
-    todos = todo.objects.all()
-    return render(request, 'home.html', {'todos': todos})
+    if request.method == 'POST':
+        form = TodoForm(request.POST or Nome)
+        if form.is_valid():
+            form.save()
+            todos = todo.objects.all()
+            return render(request, 'home.html', {'todos': todos})
 
 def about(request):
     context = {'name': 'Marcus Vinicius', 'age': 23}
