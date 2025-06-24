8ed178a498295e1683b0971a998fb9a935a758d4
Marcus <marcusbol28@gmail.com>
2025-06-24 11:42:26 -0300

    Adicionado

--- DjangoDB3/app/todo/urls.py
+++ DjangoDB3/app/todo/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
+urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('adicionar/', views.adicionar_todo, name='adicionar_todo'),
+    path('update/<int:id>/', views.update_todo, name='update'),
+    path('delete/<int:id>/', views.delete_todo, name='delete'),
 ]
 
--- DjangoDB3/app/todo/views.py
+++ DjangoDB3/app/todo/views.py
@@ -1,9 +1,42 @@
-from django.shortcuts import render
-from .models import todo
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import todo 
+from .forms import TodoForm 
 
 def home(request):
-    todos = todo.objects.all
-    return render(request, 'home.html',{'todos': todos})
+    todos = todo.objects.all() 
+    return render(request, 'home.html', {'todos': todos})
 
 def about(request):
     return render(request, 'about.html', {'name': 'Marcus'})
+
+def update(request):
+    return render(request, 'update.html' )
+
+def update_todo(request, id):
+    tarefa = get_object_or_404(todo, id=id)
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=tarefa)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TodoForm(instance=tarefa)
+    return render(request, 'update.html', {'form': form})
+
+def delete_todo(request, id):
+    tarefa = get_object_or_404(todo, id=id)
+    tarefa.delete()
+    return redirect('home')
+
+def adicionar(request):
+    return render(request, 'adicionar.html')
+
+def adicionar_todo(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TodoForm()
+    return render(request, 'adicionar.html', {'form': form})
